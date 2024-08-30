c13c69153c7264c222c0a351ca1d847fe1e7f9d2
Rohith286 <rohithsai2006@gmail.com>
2024-08-30 17:26:00 +0530

    Add files via upload

--- Matgeo-1/mat1.c
+++ Matgeo-1/mat1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <math.h>
 
+// Define the structure for a 2D vector
+typedef struct {
+    double x;
+    double y;
+} Vector;
+
+// Function to compute the resultant vector
+Vector calculate_resultant_vector(Vector wind, Vector boat) {
+    Vector resultant;
+    resultant.x = wind.x; // x component remains the same as wind
+    resultant.y = wind.y - boat.y; // y component is wind - boat
+    return resultant;
+}
+
+// Function to compute the angle using arctan
+double calculate_angle(Vector wind, Vector boat) {
+    Vector resultant = calculate_resultant_vector(wind, boat);
+    return atan(resultant.y / resultant.x); // Angle in radians
+}
